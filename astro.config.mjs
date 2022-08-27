bd5e3aa3061dd1010fa64905257287e97105e7a3
David Jaimes <dj@davidjaimes.com>
2022-08-27 16:14:23 -0700

    add first markdown post with h2 formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    config: { applyBaseStyles: true },
+  })],
   site: "https://davidjaimes.github.io"
 });
