a824e1d35b764a5b4e2cdf37c63be01a3861a1b7
David Jaimes <dj@davidjaimes.com>
2023-03-26 18:38:58 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    config: { applyBaseStyles: true },
-  })],
+    config: {
+      applyBaseStyles: true
+    }
+  }), react()],
   site: "https://davidjaimes.github.io",
   markdown: {
     shikiConfig: {
@@ -13,5 +17,5 @@ export default defineConfig({
     },
     remarkPlugins: ['remark-math'],
     rehypePlugins: ['rehype-katex']
-  },
+  }
 });
