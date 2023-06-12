efa81cd54e7b5c5b6a34d9703134285f12cc5874
David Jaimes <dj@davidjaimes.com>
2023-06-12 00:10:42 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: "public",
+  publicDir: "static",
+
   experimental: { assets: true },
   integrations: [tailwind({
     config: {
